385d647938ed396f711f75c6e08dc70ba50a6cd3
Shankarv19bcr <shankarv292002@gmail.com>
2025-06-25 18:16:59 +0530

    fix: override SRE validation in SO

--- lifelong_erpnext/lifelong_erpnext/custom_server_scripts/custom_pick_list.py
+++ lifelong_erpnext/lifelong_erpnext/custom_server_scripts/custom_pick_list.py
@@ -90,6 +90,27 @@ class CustomPickList(PickList):
 		if save:
 			self.save()
 
+	def validate_sales_order(self):
+		"""Raises an exception if the `Sales Order` has reserved stock."""
+		# And Ignore SRE which created via Pick List
+
+		if self.purpose != "Delivery":
+			return
+
+		so_list = set(location.sales_order for location in self.locations if location.sales_order)
+
+		if so_list:
+			for so in so_list:
+				so_doc = frappe.get_doc("Sales Order", so)
+				for item in so_doc.items:
+					frappe.errprint(item)
+					if item.stock_reserved_qty > 0 and not frappe.db.exists("Stock Reservation Entry", {"docstatus": 1, "voucher_type": "Sales Order", "voucher_no": item.parent, "from_voucher_type": "Pick List"}):
+						frappe.throw(
+							_(
+								"Cannot create a pick list for Sales Order {0} because it has reserved stock. Please unreserve the stock in order to create a pick list."
+							).format(frappe.bold(so))
+						)
+
 def get_available_item_locations(item_code, from_warehouses, required_qty, company, item_doc, ignore_validation=False):
 	locations = []
 	has_serial_no  = frappe.get_cached_value('Item', item_code, 'has_serial_no')
