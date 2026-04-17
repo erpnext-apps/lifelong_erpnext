d207374c8cd1757676fdfeca8669b4e535dbb7b2
rohitkumar8848 <rohitkumar@8848digital.com>
2026-04-17 18:12:18 +0530

    fix: modified shelf naming series

--- lifelong_erpnext/lifelong_erpnext/doctype/shelf/shelf.py
+++ lifelong_erpnext/lifelong_erpnext/doctype/shelf/shelf.py
@@ -7,6 +7,7 @@ from frappe.model.document import Document
 
 class Shelf(Document):
 	def validate(self):
+		self.validate_unique_shelf()
 		if not self.is_new():
 			self.check_stock_exsts()
 
@@ -19,3 +20,46 @@ class Shelf(Document):
 
 			if sle_exists:
 				frappe.throw(_(f'The stock ledgers exists against the warehouse {bold(warehouse)}'))
+	
+	def autoname(self):
+		if not self.rack:
+			frappe.throw("Rack is required")
+
+		zone = frappe.db.get_value("Rack", self.rack, "zone")
+
+		if not zone:
+			frappe.throw(f"Zone not found for Rack {self.rack}")
+
+		if not self.warehouse:
+			frappe.throw("Warehouse is required")
+
+		if not self.shelf_name:
+			frappe.throw("Shelf Name is required")
+
+		self.name = (
+			f"{self.shelf_name}-"
+			f"{self.rack}-"
+			f"{zone}-"
+			f"{self.warehouse}"
+		)
+
+	
+	def validate_unique_shelf(self):
+		zone = frappe.db.get_value("Rack", self.rack, "zone")
+
+		existing = frappe.db.exists(
+			"Shelf",
+			{
+				"warehouse": self.warehouse,
+				"rack": self.rack,
+				"shelf_name": self.shelf_name,
+				"name": ["!=", self.name]
+			}
+		)
+
+		if existing:
+			frappe.throw(
+				f"Shelf '{self.shelf_name}' already exists in "
+				f"Rack '{self.rack}', Zone '{zone}', "
+				f"Warehouse '{self.warehouse}'"
+			)
