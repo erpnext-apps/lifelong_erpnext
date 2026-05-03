c499a14b9ad522e6dfcaae9e3e04d76f60cd586d
Hassan Shaikh <hassan@8848digital.com>
2026-05-03 16:07:23 +0000

    feat: added shelf fields and auto naming

--- lifelong_erpnext/lifelong_erpnext/doctype/shelf/shelf.py
+++ lifelong_erpnext/lifelong_erpnext/doctype/shelf/shelf.py
@@ -7,6 +7,7 @@ from frappe.model.document import Document
 
 class Shelf(Document):
 	def validate(self):
+		self.validate_unique_shelf()
 		if not self.is_new():
 			self.check_stock_exsts()
 
@@ -19,3 +20,41 @@ class Shelf(Document):
 
 			if sle_exists:
 				frappe.throw(_(f'The stock ledgers exists against the warehouse {bold(warehouse)}'))
+	
+	def autoname(self):
+
+		if not self.shelf_name:
+			frappe.throw("Shelf Name is required")
+
+		if not self.rack:
+			frappe.throw("Rack is required")
+
+		# Validate rack has zone (data integrity check)
+		zone = frappe.get_cached_value("Rack", self.rack, "zone")
+
+		if not zone:
+			frappe.throw(f"Zone not set in Rack {self.rack}")
+
+		if not self.warehouse:
+			frappe.throw("Warehouse is required")
+
+		# FINAL CLEAN NAME
+		self.name = f"{self.shelf_name}-{self.rack}"
+
+	
+
+	def validate_unique_shelf(self):
+
+		existing = frappe.db.exists(
+			"Shelf",
+			{
+				"rack": self.rack,
+				"shelf_name": self.shelf_name,
+				"name": ["!=", self.name]
+			}
+		)
+
+		if existing:
+			frappe.throw(
+				f"Shelf '{self.shelf_name}' already exists in Rack '{self.rack}'"
+			)
